b32e409e53d6a6cc20edff448057d30061fee415
wvbwagner <wvbwagner@gmail.com>
2023-08-24 09:21:07 -0300

    Adicionados pacote OS para verificação da LANG e impressão da mensagem de acordo com a LANG econtrada

--- hello.py
+++ hello.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
+'''
+Hello World Multi Linguas
+
+Dependendo da lingua configurada no
+ambiente, o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+    ou
+    ./hello.py
+'''
+
+__version__ = "0.0.1"
+__author__ = "Wagner Botelho"
+__license__ = "unlicensed"
 
 import os
 
-teste = os.environ['PATH']
+current_language = os.getenv("LANG")[:5]
 
-print("Hello Linux")
-print(teste)
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo"
+
+print(msg)
